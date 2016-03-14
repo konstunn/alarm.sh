64ee6d755fc695fe7f5d0de41d7c7ebe64c8ddea
Konstantin Gorbunov <konstunn@ngs.ru>
2016-03-14 22:09:27 +0600

    Implements text menu interface. Adds config file. Modifies set time function to return status code instead of exit. Fixes log function output. Adds comments.

--- alarm.sh
+++ alarm.sh
@@ -31,25 +31,38 @@ TRACK="/media/konstunn/data/mamedia/recovered_music/Delinquent Habits - Return O
 TRACK="/home/konstunn/Music/Delinquent Habits - Return Of The Tres (Instrumental).mp3"
 
 LOG_FILE="./alarm.log"
+CONF_FILE="./alarm.conf"
 
-TIMEOUT="5m" 
+TIMEOUT="5m"
 SOUND_VOLUME="58"
 
-PLAY_NOW="0"
+PLAY_NOW=0
+CONFIG=0
 
 export LC_TIME="en_US.utf8"
 
 function log {
-	echo "$(date +'%b %d %T') localhost$2 $(basename $0)$1" >> $LOG_FILE 
+	echo "$(date +'%b %d %T') localhost: $(basename $0)$1" >> $LOG_FILE
 }
 
 function print_help {
 	echo -e "\nUsage:\n `basename $0` [options]"
 	echo -e "\nOptions: \
-		\n --track  <track> (Default: \"-\") \
-		\n --time   <time>  (Default: \"-\") \
 		\n --play-now | -p  \
+		\n --config | -c \
 		\n --help | -h \n"
+}
+
+function read_track_conf {
+	echo ""
+}
+
+function write_track_conf {
+	echo ""
+}
+
+function get_time {
+	echo ""
 }
 
 # TODO: add disabling feature
@@ -60,14 +73,14 @@ function set_time {
 		# TODO: redirect to stderr ?
 		echo $(basename $0): invalid alarm time '$1'
 		echo $(basename $0): terminating...
-		exit 1
+		return 1
 	fi
 
 	# extract minutes and hours from $1
 	HOURS=$(echo $1 | awk -F':' '{print $1}')
 	MINUTES=$(echo $1 | awk -F':' '{print $2}')
 
-	DT_MIN=5 # minutes for rest between wake and cron job
+	DT_MIN=5 # minutes to idle between wake and cron job
 
 	# unix time for rtcwake
 	WAKE_TIME=$((`date -d "$1" +%s` - $DT_MIN*60))
@@ -91,25 +104,28 @@ function set_time {
 	# TODO: check if it is already set,
 	# if yes, compare. if set later, set new,
 	# else leave it
+	echo "sudo rtcwake -m no -t \$WAKE_TIME"
 	sudo rtcwake -m no -t $WAKE_TIME
+
+	return 0
 }
 
-# TODO: parse command line arguments
-
-OPTS="+h,p"
-LONG_OPTS="help,track:,time:,play-now"
-
-if [ $# == 0 ] ; then # TODO: redirect to stderr ?
+# if no arguments, claim and exit
+if [ $# -eq 0 ] ; then # TODO: redirect to stderr ?
 	echo "`basename $0`: no options specified"
 	echo "`basename $0`: for list of options specify '--help' or '-h' option."
 	echo "`basename $0`: terminating..."
 	exit 1 
 fi
 
-ARGS=`getopt -o '+h,p' --long "$LONG_OPTS" \
+OPTS=+h,p,c
+LONG_OPTS="help,play-now,config"
+
+ARGS=`getopt -o $OPTS --long $LONG_OPTS \
      -n $(basename $0) -- "$@"`
 
-if [ $? != 0 ] ; then # TODO: redirect to stderr ?
+# if getopt returned error, claim and exit
+if [ $? -ne 0 ] ; then # TODO: redirect to stderr ?
 	echo "`basename $0`: specify '--help' or '-h' option for help."
 	echo "`basename $0`: terminating..."
 	exit 1
@@ -117,17 +133,17 @@ fi
 
 eval set -- "$ARGS"
 
+# parse command line arguments
 while true ; do
-	case "$1" in
-		--play-now | -p)	PLAY_NOW=1 ; shift 1 ;;
-		--time)				set_time $2 ; shift 2 ;;
-		# TODO: write track to config file
-		--track)		TRACK="$2" ; PLAY_NOW=1 ; shift 2 ;;
+	case "$1" in --play-now | -p)	PLAY_NOW=1 ; shift ;;
+		--config | --configure | -c)
+			CONFIG=1 ; shift ;;
 		--help | -h)	print_help; exit 0 ;;
 		--)				shift ; break ;;
 	esac
 done
 
+# claim if extra arguments and exit
 if [ $# -gt 0 ] ; then # TODO: redirect to stderr ?
 	echo "`basename $0`: extra arguments \"$@\""
 	echo "`basename $0`: specify '--help' or '-h' option for help."
@@ -135,11 +151,65 @@ if [ $# -gt 0 ] ; then # TODO: redirect to stderr ?
 	exit 1
 fi
 
+# invoke text menu
+if [ $CONFIG -ne 0 ] ; then
+
+	# read track from conf
+	if [ -r $CONF_FILE ] ; then
+		source $CONF_FILE
+	else
+		echo $CONF_FILE : no such a readable config file
+		exit 1
+	fi
+
+	# get time from crontab
+	MINUTES=`crontab -l | sed -ne "/alarm.sh/p" | awk '{print $1}'`
+	HOURS=`crontab -l | sed -ne "/alarm.sh/p" | awk '{print $2}'`
+	TIME=$HOURS:$MINUTES
+
+	while true ; do
+		echo -e "1. set time (\"$TIME\")"
+		echo -e "2. set audio file (\"$TRACK\")"
+		echo	"3. play"
+		echo	"4. exit"
+		echo -en "\nEnter your choice: "
+
+		read DECISION
+		echo ""
+
+		case "$DECISION" in
+			1)
+				read -p "Enter time in 'hh:mm' format: " VAR
+				set_time $VAR
+				if [ $? -ne 0 ] ; then echo Time was not set 
+				else TIME=$VAR ; fi
+			;;
+			2)
+				read -ep "Enter path to audio file: " VAR
+				if [ -r $VAR ] ; then 
+					TRACK=$VAR
+					# write to conf
+					echo TRACK="$TRACK" > $CONF_FILE
+				else
+					echo $VAR : no such readable a file
+				fi
+				read -p "Press Enter.. "
+			;;
+			3) PLAY_NOW=1 ; break ;;
+			4) exit 0 ;;
+			*) read -p "Invalid input. Press Enter.. " ;;
+		esac
+
+		clear
+
+	done
+fi
+
 if [ $PLAY_NOW -eq 0 ] ; then exit 0; fi
 
 # play now
 
-log " started." ":"
+log " started."
 
 while true
 do 
